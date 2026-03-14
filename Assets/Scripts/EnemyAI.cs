d5eebb44c1c2aa9ed95e58d862b1057bb2e89161
Beginner10617 <wasihusain23@iitk.ac.in>
2026-03-14 14:38:42 +0530

    enemyAI with animation

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -12,8 +12,9 @@ public class EnemyAI : MonoBehaviour
     }
 
     public State currentState;
-
+    Animator animator;
     public Transform player;
+    bool canReach = false;
 
     public float detectionRange = 12f;
     public float attackRange = 2f;
@@ -23,15 +24,41 @@ public class EnemyAI : MonoBehaviour
 
     private NavMeshAgent agent;
 
+    // Helper function
+    bool HasPathToPlayer()
+    {
+        NavMeshPath path = new NavMeshPath();
+
+        if(agent.CalculatePath(player.position, path))
+        {
+            return path.status == NavMeshPathStatus.PathComplete;
+        }
+
+        return false;
+    }
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         currentState = State.Idle;
+        animator = GetComponent<Animator>();
     }
+
+    float pathCheckTimer;
 
     void Update()
     {
+        //Debug.Log(agent.isOnNavMesh);
+        Debug.Log(canReach);
+        pathCheckTimer += Time.deltaTime;
+
+         if(pathCheckTimer >= 0.5f)
+        {
+            canReach = HasPathToPlayer();
+            pathCheckTimer = 0f;
+        }
+
         if(currentState == State.Dead)
             return;
 
@@ -57,6 +84,8 @@ public class EnemyAI : MonoBehaviour
     {
         agent.isStopped = true;
 
+        animator.SetFloat("Speed", 0f);
+
         if(distance < detectionRange)
         {
             currentState = State.Chase;
@@ -65,16 +94,31 @@ public class EnemyAI : MonoBehaviour
 
     void ChaseState(float distance)
     {
+
+        if(!canReach)
+        {
+            agent.isStopped = true;
+            animator.SetFloat("Speed", 0.3f); // idle with alert
+            return;
+        }
+
         agent.isStopped = false;
         agent.SetDestination(player.position);
+
+        if(distance < 8f)
+        {
+            agent.speed = 4.5f;
+            animator.SetFloat("Speed", 1f); // run
+        }
+        else
+        {
+            agent.speed = 2f;
+            animator.SetFloat("Speed", 0.5f); // walk
+        }
 
         if(distance < attackRange)
         {
             currentState = State.Attack;
-        }
-        else if(distance > detectionRange * 1.5f)
-        {
-            currentState = State.Idle;
         }
     }
 
@@ -84,8 +128,11 @@ public class EnemyAI : MonoBehaviour
 
         transform.LookAt(player);
 
+        animator.SetFloat("Speed", 0f);
+
         if(Time.time > lastAttackTime + attackCooldown)
         {
+            animator.SetTrigger("Attack");
             Attack();
             lastAttackTime = Time.time;
         }
@@ -105,8 +152,11 @@ public class EnemyAI : MonoBehaviour
     public void Die()
     {
         currentState = State.Dead;
+
         agent.isStopped = true;
 
-        Destroy(gameObject, 2f);
+        animator.SetBool("Dead", true);
+
+        Destroy(gameObject, 3f);
     }
 }
